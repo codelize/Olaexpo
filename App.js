52f342333a6664de3a8bcc30d111130c9bce2c1e
codelize <lflizier@gmail.com>
2024-08-18 19:58:31 -0300

    Aplicativo utilizando API de CEP desenvolvido em React-Native

--- App.js
+++ App.js
@@ -1,230 +1,48 @@
-import React from 'react-native';
-import { Text}  from "react-native";
+import React, { Image, SafeAreaView, View, Text, StatusBar, TextInput, TouchableOpacity } from 'react-native';
+import { styles } from './style.js';
+import { useState } from 'react';
 
 function App() {
-  return <Text>ALuraaaaaaaaaaaaaaaaaaaaaaa
+  const [cep, setCep] = useState("");
+  const [resultado, setResultado] = useState("");
 
-ALuraaaaaaaaaaaaaaaaaaaaaaa
-ALuraaaaaaaaaaaaaaaaaaaaaaa
-ALuraaaaaaaaaaaaaaaaaaaaaaa
-ALuraaaaaaaaaaaaaaaaaaaaaaaALuraaaaaaaaaaaaaaaaaaaaaaa
-ALuraaaaaaaaaaaaaaaaaaaaaaa
-ALuraaaaaaaaaaaaaaaaaaaaaaa
-ALuraaaaaaaaaaaaaaaaaaaaaaaALuraaaaaaaaaaaaaaaaaaaaaaa
-ALuraaaaaaaaaaaaaaaaaaaaaaa
-ALuraaaaaaaaaaaaaaaaaaaaaaa
-ALuraaaaaaaaaaaaaaaaaaaaaaaALuraaaaaaaaaaaaaaaaaaaaaaa
-ALuraaaaaaaaaaaaaaaaaaaaaaa
-ALuraaaaaaaaaaaaaaaaaaaaaaa
-ALuraaaaaaaaaaaaaaaaaaaaaaaALuraaaaaaaaaaaaaaaaaaaaaaa
-ALuraaaaaaaaaaaaaaaaaaaaaaa
-ALuraaaaaaaaaaaaaaaaaaaaaaa
-ALuraaaaaaaaaaaaaaaaaaaaaaaALuraaaaaaaaaaaaaaaaaaaaaaa
-ALuraaaaaaaaaaaaaaaaaaaaaaa
-ALuraaaaaaaaaaaaaaaaaaaaaaa
-ALuraaaaaaaaaaaaaaaaaaaaaaaALuraaaaaaaaaaaaaaaaaaaaaaa
-ALuraaaaaaaaaaaaaaaaaaaaaaa
-ALuraaaaaaaaaaaaaaaaaaaaaaa
-ALuraaaaaaaaaaaaaaaaaaaaaaaALuraaaaaaaaaaaaaaaaaaaaaaa
-ALuraaaaaaaaaaaaaaaaaaaaaaa
-ALuraaaaaaaaaaaaaaaaaaaaaaa
-ALuraaaaaaaaaaaaaaaaaaaaaaaALuraaaaaaaaaaaaaaaaaaaaaaa
-ALuraaaaaaaaaaaaaaaaaaaaaaa
-ALuraaaaaaaaaaaaaaaaaaaaaaa
-ALuraaaaaaaaaaaaaaaaaaaaaaaALuraaaaaaaaaaaaaaaaaaaaaaa
-ALuraaaaaaaaaaaaaaaaaaaaaaa
-ALuraaaaaaaaaaaaaaaaaaaaaaa
-ALuraaaaaaaaaaaaaaaaaaaaaaaALuraaaaaaaaaaaaaaaaaaaaaaa
-ALuraaaaaaaaaaaaaaaaaaaaaaa
-ALuraaaaaaaaaaaaaaaaaaaaaaa
-ALuraaaaaaaaaaaaaaaaaaaaaaaALuraaaaaaaaaaaaaaaaaaaaaaa
-ALuraaaaaaaaaaaaaaaaaaaaaaa
-ALuraaaaaaaaaaaaaaaaaaaaaaa
-ALuraaaaaaaaaaaaaaaaaaaaaaaALuraaaaaaaaaaaaaaaaaaaaaaa
-ALuraaaaaaaaaaaaaaaaaaaaaaa
-ALuraaaaaaaaaaaaaaaaaaaaaaa
-ALuraaaaaaaaaaaaaaaaaaaaaaaALuraaaaaaaaaaaaaaaaaaaaaaa
-ALuraaaaaaaaaaaaaaaaaaaaaaa
-ALuraaaaaaaaaaaaaaaaaaaaaaa
-ALuraaaaaaaaaaaaaaaaaaaaaaaALuraaaaaaaaaaaaaaaaaaaaaaa
-ALuraaaaaaaaaaaaaaaaaaaaaaa
-ALuraaaaaaaaaaaaaaaaaaaaaaa
-ALuraaaaaaaaaaaaaaaaaaaaaaaALuraaaaaaaaaaaaaaaaaaaaaaa
-ALuraaaaaaaaaaaaaaaaaaaaaaa
-ALuraaaaaaaaaaaaaaaaaaaaaaa
-ALuraaaaaaaaaaaaaaaaaaaaaaaALuraaaaaaaaaaaaaaaaaaaaaaa
-ALuraaaaaaaaaaaaaaaaaaaaaaa
-ALuraaaaaaaaaaaaaaaaaaaaaaa
-ALuraaaaaaaaaaaaaaaaaaaaaaaALuraaaaaaaaaaaaaaaaaaaaaaa
-ALuraaaaaaaaaaaaaaaaaaaaaaa
-ALuraaaaaaaaaaaaaaaaaaaaaaa
-ALuraaaaaaaaaaaaaaaaaaaaaaaALuraaaaaaaaaaaaaaaaaaaaaaa
-ALuraaaaaaaaaaaaaaaaaaaaaaa
-ALuraaaaaaaaaaaaaaaaaaaaaaa
-ALuraaaaaaaaaaaaaaaaaaaaaaaALuraaaaaaaaaaaaaaaaaaaaaaa
-ALuraaaaaaaaaaaaaaaaaaaaaaa
-ALuraaaaaaaaaaaaaaaaaaaaaaa
-ALuraaaaaaaaaaaaaaaaaaaaaaaALuraaaaaaaaaaaaaaaaaaaaaaa
-ALuraaaaaaaaaaaaaaaaaaaaaaa
-ALuraaaaaaaaaaaaaaaaaaaaaaa
-ALuraaaaaaaaaaaaaaaaaaaaaaaALuraaaaaaaaaaaaaaaaaaaaaaa
-ALuraaaaaaaaaaaaaaaaaaaaaaa
-ALuraaaaaaaaaaaaaaaaaaaaaaa
-ALuraaaaaaaaaaaaaaaaaaaaaaaALuraaaaaaaaaaaaaaaaaaaaaaa
-ALuraaaaaaaaaaaaaaaaaaaaaaa
-ALuraaaaaaaaaaaaaaaaaaaaaaa
-ALuraaaaaaaaaaaaaaaaaaaaaaaALuraaaaaaaaaaaaaaaaaaaaaaa
-ALuraaaaaaaaaaaaaaaaaaaaaaa
-ALuraaaaaaaaaaaaaaaaaaaaaaa
-ALuraaaaaaaaaaaaaaaaaaaaaaaALuraaaaaaaaaaaaaaaaaaaaaaa
-ALuraaaaaaaaaaaaaaaaaaaaaaa
-ALuraaaaaaaaaaaaaaaaaaaaaaa
-ALuraaaaaaaaaaaaaaaaaaaaaaaALuraaaaaaaaaaaaaaaaaaaaaaa
-ALuraaaaaaaaaaaaaaaaaaaaaaa
-ALuraaaaaaaaaaaaaaaaaaaaaaa
-ALuraaaaaaaaaaaaaaaaaaaaaaaALuraaaaaaaaaaaaaaaaaaaaaaa
-ALuraaaaaaaaaaaaaaaaaaaaaaa
-ALuraaaaaaaaaaaaaaaaaaaaaaa
-ALuraaaaaaaaaaaaaaaaaaaaaaaALuraaaaaaaaaaaaaaaaaaaaaaa
-ALuraaaaaaaaaaaaaaaaaaaaaaa
-ALuraaaaaaaaaaaaaaaaaaaaaaa
-ALuraaaaaaaaaaaaaaaaaaaaaaaALuraaaaaaaaaaaaaaaaaaaaaaa
-ALuraaaaaaaaaaaaaaaaaaaaaaa
-ALuraaaaaaaaaaaaaaaaaaaaaaa
-ALuraaaaaaaaaaaaaaaaaaaaaaaALuraaaaaaaaaaaaaaaaaaaaaaa
-ALuraaaaaaaaaaaaaaaaaaaaaaa
-ALuraaaaaaaaaaaaaaaaaaaaaaa
-ALuraaaaaaaaaaaaaaaaaaaaaaaALuraaaaaaaaaaaaaaaaaaaaaaa
-ALuraaaaaaaaaaaaaaaaaaaaaaa
-ALuraaaaaaaaaaaaaaaaaaaaaaa
-ALuraaaaaaaaaaaaaaaaaaaaaaaALuraaaaaaaaaaaaaaaaaaaaaaa
-ALuraaaaaaaaaaaaaaaaaaaaaaa
-ALuraaaaaaaaaaaaaaaaaaaaaaa
-ALuraaaaaaaaaaaaaaaaaaaaaaaALuraaaaaaaaaaaaaaaaaaaaaaa
-ALuraaaaaaaaaaaaaaaaaaaaaaa
-ALuraaaaaaaaaaaaaaaaaaaaaaa
-ALuraaaaaaaaaaaaaaaaaaaaaaaALuraaaaaaaaaaaaaaaaaaaaaaa
-ALuraaaaaaaaaaaaaaaaaaaaaaa
-ALuraaaaaaaaaaaaaaaaaaaaaaa
-ALuraaaaaaaaaaaaaaaaaaaaaaaALuraaaaaaaaaaaaaaaaaaaaaaa
-ALuraaaaaaaaaaaaaaaaaaaaaaa
-ALuraaaaaaaaaaaaaaaaaaaaaaa
-ALuraaaaaaaaaaaaaaaaaaaaaaaALuraaaaaaaaaaaaaaaaaaaaaaa
-ALuraaaaaaaaaaaaaaaaaaaaaaa
-ALuraaaaaaaaaaaaaaaaaaaaaaa
-ALuraaaaaaaaaaaaaaaaaaaaaaaALuraaaaaaaaaaaaaaaaaaaaaaa
-ALuraaaaaaaaaaaaaaaaaaaaaaa
-ALuraaaaaaaaaaaaaaaaaaaaaaa
-ALuraaaaaaaaaaaaaaaaaaaaaaaALuraaaaaaaaaaaaaaaaaaaaaaa
-ALuraaaaaaaaaaaaaaaaaaaaaaa
-ALuraaaaaaaaaaaaaaaaaaaaaaa
-ALuraaaaaaaaaaaaaaaaaaaaaaaALuraaaaaaaaaaaaaaaaaaaaaaa
-ALuraaaaaaaaaaaaaaaaaaaaaaa
-ALuraaaaaaaaaaaaaaaaaaaaaaa
-ALuraaaaaaaaaaaaaaaaaaaaaaaALuraaaaaaaaaaaaaaaaaaaaaaa
-ALuraaaaaaaaaaaaaaaaaaaaaaa
-ALuraaaaaaaaaaaaaaaaaaaaaaa
-ALuraaaaaaaaaaaaaaaaaaaaaaaALuraaaaaaaaaaaaaaaaaaaaaaa
-ALuraaaaaaaaaaaaaaaaaaaaaaa
-ALuraaaaaaaaaaaaaaaaaaaaaaa
-ALuraaaaaaaaaaaaaaaaaaaaaaaALuraaaaaaaaaaaaaaaaaaaaaaa
-ALuraaaaaaaaaaaaaaaaaaaaaaa
-ALuraaaaaaaaaaaaaaaaaaaaaaa
-ALuraaaaaaaaaaaaaaaaaaaaaaaALuraaaaaaaaaaaaaaaaaaaaaaa
-ALuraaaaaaaaaaaaaaaaaaaaaaa
-ALuraaaaaaaaaaaaaaaaaaaaaaa
-ALuraaaaaaaaaaaaaaaaaaaaaaaALuraaaaaaaaaaaaaaaaaaaaaaa
-ALuraaaaaaaaaaaaaaaaaaaaaaa
-ALuraaaaaaaaaaaaaaaaaaaaaaa
-ALuraaaaaaaaaaaaaaaaaaaaaaaALuraaaaaaaaaaaaaaaaaaaaaaa
-ALuraaaaaaaaaaaaaaaaaaaaaaa
-ALuraaaaaaaaaaaaaaaaaaaaaaa
-ALuraaaaaaaaaaaaaaaaaaaaaaaALuraaaaaaaaaaaaaaaaaaaaaaa
-ALuraaaaaaaaaaaaaaaaaaaaaaa
-ALuraaaaaaaaaaaaaaaaaaaaaaa
-ALuraaaaaaaaaaaaaaaaaaaaaaaALuraaaaaaaaaaaaaaaaaaaaaaa
-ALuraaaaaaaaaaaaaaaaaaaaaaa
-ALuraaaaaaaaaaaaaaaaaaaaaaa
-ALuraaaaaaaaaaaaaaaaaaaaaaaALuraaaaaaaaaaaaaaaaaaaaaaa
-ALuraaaaaaaaaaaaaaaaaaaaaaa
-ALuraaaaaaaaaaaaaaaaaaaaaaa
-ALuraaaaaaaaaaaaaaaaaaaaaaaALuraaaaaaaaaaaaaaaaaaaaaaa
-ALuraaaaaaaaaaaaaaaaaaaaaaa
-ALuraaaaaaaaaaaaaaaaaaaaaaa
-ALuraaaaaaaaaaaaaaaaaaaaaaaALuraaaaaaaaaaaaaaaaaaaaaaa
-ALuraaaaaaaaaaaaaaaaaaaaaaa
-ALuraaaaaaaaaaaaaaaaaaaaaaa
-ALuraaaaaaaaaaaaaaaaaaaaaaaALuraaaaaaaaaaaaaaaaaaaaaaa
-ALuraaaaaaaaaaaaaaaaaaaaaaa
-ALuraaaaaaaaaaaaaaaaaaaaaaa
-ALuraaaaaaaaaaaaaaaaaaaaaaaALuraaaaaaaaaaaaaaaaaaaaaaa
-ALuraaaaaaaaaaaaaaaaaaaaaaa
-ALuraaaaaaaaaaaaaaaaaaaaaaa
-ALuraaaaaaaaaaaaaaaaaaaaaaaALuraaaaaaaaaaaaaaaaaaaaaaa
-ALuraaaaaaaaaaaaaaaaaaaaaaa
-ALuraaaaaaaaaaaaaaaaaaaaaaa
-ALuraaaaaaaaaaaaaaaaaaaaaaaALuraaaaaaaaaaaaaaaaaaaaaaa
-ALuraaaaaaaaaaaaaaaaaaaaaaa
-ALuraaaaaaaaaaaaaaaaaaaaaaa
-ALuraaaaaaaaaaaaaaaaaaaaaaaALuraaaaaaaaaaaaaaaaaaaaaaa
-ALuraaaaaaaaaaaaaaaaaaaaaaa
-ALuraaaaaaaaaaaaaaaaaaaaaaa
-ALuraaaaaaaaaaaaaaaaaaaaaaaALuraaaaaaaaaaaaaaaaaaaaaaa
-ALuraaaaaaaaaaaaaaaaaaaaaaa
-ALuraaaaaaaaaaaaaaaaaaaaaaa
-ALuraaaaaaaaaaaaaaaaaaaaaaaALuraaaaaaaaaaaaaaaaaaaaaaa
-ALuraaaaaaaaaaaaaaaaaaaaaaa
-ALuraaaaaaaaaaaaaaaaaaaaaaa
-ALuraaaaaaaaaaaaaaaaaaaaaaaALuraaaaaaaaaaaaaaaaaaaaaaa
-ALuraaaaaaaaaaaaaaaaaaaaaaa
-ALuraaaaaaaaaaaaaaaaaaaaaaa
-ALuraaaaaaaaaaaaaaaaaaaaaaaALuraaaaaaaaaaaaaaaaaaaaaaa
-ALuraaaaaaaaaaaaaaaaaaaaaaa
-ALuraaaaaaaaaaaaaaaaaaaaaaa
-ALuraaaaaaaaaaaaaaaaaaaaaaaALuraaaaaaaaaaaaaaaaaaaaaaa
-ALuraaaaaaaaaaaaaaaaaaaaaaa
-ALuraaaaaaaaaaaaaaaaaaaaaaa
-ALuraaaaaaaaaaaaaaaaaaaaaaaALuraaaaaaaaaaaaaaaaaaaaaaa
-ALuraaaaaaaaaaaaaaaaaaaaaaa
-ALuraaaaaaaaaaaaaaaaaaaaaaa
-ALuraaaaaaaaaaaaaaaaaaaaaaaALuraaaaaaaaaaaaaaaaaaaaaaa
-ALuraaaaaaaaaaaaaaaaaaaaaaa
-ALuraaaaaaaaaaaaaaaaaaaaaaa
-ALuraaaaaaaaaaaaaaaaaaaaaaaALuraaaaaaaaaaaaaaaaaaaaaaa
-ALuraaaaaaaaaaaaaaaaaaaaaaa
-ALuraaaaaaaaaaaaaaaaaaaaaaa
-ALuraaaaaaaaaaaaaaaaaaaaaaaALuraaaaaaaaaaaaaaaaaaaaaaa
-ALuraaaaaaaaaaaaaaaaaaaaaaa
-ALuraaaaaaaaaaaaaaaaaaaaaaa
-ALuraaaaaaaaaaaaaaaaaaaaaaaALuraaaaaaaaaaaaaaaaaaaaaaa
-ALuraaaaaaaaaaaaaaaaaaaaaaa
-ALuraaaaaaaaaaaaaaaaaaaaaaa
-ALuraaaaaaaaaaaaaaaaaaaaaaaALuraaaaaaaaaaaaaaaaaaaaaaa
-ALuraaaaaaaaaaaaaaaaaaaaaaa
-ALuraaaaaaaaaaaaaaaaaaaaaaa
-ALuraaaaaaaaaaaaaaaaaaaaaaaALuraaaaaaaaaaaaaaaaaaaaaaa
-ALuraaaaaaaaaaaaaaaaaaaaaaa
-ALuraaaaaaaaaaaaaaaaaaaaaaa
-ALuraaaaaaaaaaaaaaaaaaaaaaaALuraaaaaaaaaaaaaaaaaaaaaaa
-ALuraaaaaaaaaaaaaaaaaaaaaaa
-ALuraaaaaaaaaaaaaaaaaaaaaaa
-ALuraaaaaaaaaaaaaaaaaaaaaaaALuraaaaaaaaaaaaaaaaaaaaaaa
-ALuraaaaaaaaaaaaaaaaaaaaaaa
-ALuraaaaaaaaaaaaaaaaaaaaaaa
-ALuraaaaaaaaaaaaaaaaaaaaaaaALuraaaaaaaaaaaaaaaaaaaaaaa
-ALuraaaaaaaaaaaaaaaaaaaaaaa
-ALuraaaaaaaaaaaaaaaaaaaaaaa
-ALuraaaaaaaaaaaaaaaaaaaaaaaALuraaaaaaaaaaaaaaaaaaaaaaa
-ALuraaaaaaaaaaaaaaaaaaaaaaa
-ALuraaaaaaaaaaaaaaaaaaaaaaa
-ALuraaaaaaaaaaaaaaaaaaaaaaaALuraaaaaaaaaaaaaaaaaaaaaaa
-ALuraaaaaaaaaaaaaaaaaaaaaaa
-ALuraaaaaaaaaaaaaaaaaaaaaaa
-ALuraaaaaaaaaaaaaaaaaaaaaaaALuraaaaaaaaaaaaaaaaaaaaaaa
-ALuraaaaaaaaaaaaaaaaaaaaaaa
-ALuraaaaaaaaaaaaaaaaaaaaaaa
-ALuraaaaaaaaaaaaaaaaaaaaaaa
-  </Text>
+  async function ConsultarCep() {
+    console.log(cep);
+
+    const request = await fetch("https://viacep.com.br/ws/" + cep + "/json/");
+    const retorno = await request.json();
+
+    setResultado("Endereço: " + retorno.logradouro);
+    
+    console.log(retorno);
+  }
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <StatusBar barStyle="dark-content"/>
+      <Image 
+        style={styles.img} 
+        source={{ uri: "https://www.climba.com.br/blog/wp-content/uploads/2016/08/Mudan%C3%A7as-no-PAc-dos-Correios-Idealize-Tecnologia-1.png" }} 
+      />
+      <Text style={styles.titulo}>Consulta CEP</Text>
+
+      <View style={styles.form}> 
+        <TextInput 
+          placeholder="Digite o CEP..." 
+          style={styles.TextInput} 
+          onChangeText={texto => setCep(texto)}
+        /> 
+        
+        <TouchableOpacity style={styles.btn} onPress={ConsultarCep}> 
+          <Text style={styles.btnText}>Consultar</Text> 
+        </TouchableOpacity>     
+      </View>
+
+      <View style={styles.form}>
+        <Text style={styles.resultado}>{resultado}</Text>
+      </View>
+    </SafeAreaView>
+  );
 }
 
 export default App;
---
+++ style.js
@@ -0,0 +1,48 @@
+export const styles = {
+    container: {
+      backgroundColor: "#103F19",
+      flex: 1,
+      alignItems: "center",
+      justifyContent: "flex-start", // Alinha os elementos no topo
+      paddingTop: 20, // Espaçamento no topo para evitar colar na borda superior
+    },
+    titulo: {
+      color: "white",  
+      fontSize: 30,
+      padding: 10, 
+      textAlign: "center",
+      marginTop: 10 // Espaçamento abaixo da imagem
+    },
+    TextInput: {
+      backgroundColor: "#ececec",
+      padding: 15,
+      width: 150,
+      borderRadius: 6,
+      marginTop: 5 // Espaçamento entre a imagem e o campo de entrada
+    },
+    form: {
+      marginTop: 5
+    },
+    resultado: {
+      fontSize: 30
+    },
+    btn: {
+        backgroundColor: "#41784C",
+      width: 150,
+      borderRadius: 50,
+      padding: 8,
+      alignItems: "center",
+      marginTop: 10
+    },
+    btnText: {
+      color: "#fff",
+      fontSize: 20 
+    },
+    img: {
+      borderRadius: 16,
+      width: 420,
+      height: 100,
+      marginTop: 10, // Pequeno espaçamento abaixo da imagem
+    }
+  };
+  
